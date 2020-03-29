365a0a8d7ac8b59a1348787cfdb3f9b03b12c755
Matt Fleming <matthewfleming8@gmail.com>
2020-03-29 18:32:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,5 @@
 def square_array(array)
-  new_numbers = array.each do |number|
-    new_numbers >> (array ** 2)
-  end
-  return new_numbers
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
